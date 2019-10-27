adf76497f1c0c92f07ae74e1980f940c739cad2a
nuatthaitest <nuatthaitest@gmail.com>
2019-10-27 10:58:19 +0800

    a

--- nuatthai/settings/settings.py
+++ nuatthai/settings/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 """
 
 import os
+import django_heroku
+
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -137,3 +139,5 @@ EMAIL_PORT = 587
 
 EMAIL_HOST_USER = 'nuatthaitest@gmail.com'
 EMAIL_HOST_PASSWORD = 'fgtqiogkmkqhcstk'
+
+django_heroku.settings(locals())
